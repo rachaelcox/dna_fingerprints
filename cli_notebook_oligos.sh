a039ddb9bb1c664233ef182f8404c14e2b387895
Rachael Cox <rmcox@hfogcomp01.ccbb.utexas.edu>
2025-09-25 17:09:20 -0500

    clean up notebook

--- cli_notebook_oligos.sh
+++ cli_notebook_oligos.sh
@@ -32,11 +32,11 @@
 
 cd /stor/work/Marcotte/project/rmcox/oligos
 mkdir {data,cmds,logs,results,figures,scripts}
-mkdir -p data/raw/{oligo_1,oligo_2,oligo_3,oligo_b,streptavidin}
-mkdir -p data/qc/{oligo_1,oligo_2,oligo_3,oligo_b,streptavidin,fastp}
-mkdir -p data/merged/{oligo_1,oligo_2,oligo_3,oligo_b,streptavidin}
-mkdir -p data/processed/{oligo_1,oligo_2,oligo_3,oligo_b,streptavidin}
-mkdir -p results/{oligo_1,oligo_2,oligo_3,oligo_b,streptavidin}/{graphs,counts,enrichment}
+mkdir -p data/raw/{oligo_1,oligo_2,oligo_3,oligo_b}
+mkdir -p data/qc/{oligo_1,oligo_2,oligo_3,oligo_b,fastp}
+mkdir -p data/merged/{oligo_1,oligo_2,oligo_3,oligo_b}
+mkdir -p data/processed/{oligo_1,oligo_2,oligo_3,oligo_b}
+mkdir -p results/{oligo_1,oligo_2,oligo_3,oligo_b}/{graphs,counts,enrichment}
 
 # view directory structure
 tree data/
@@ -50,11 +50,6 @@ ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/*Oligo1* data/raw/oligo
 ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/*Oligo2* data/raw/oligo_2
 ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/*Oligo3* data/raw/oligo_3
 ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/*OligoBlind* data/raw/oligo_b
-
-# streptavidin
-ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/*Lib12-SA* data/raw/streptavidin
-ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/*Probe12-SA* data/raw/streptavidin
-ln -s /stor/work/Ellington/scratch/Zac/PLA/data/new_data/JA22066*SA* data/raw/streptavidin
 
 # =======================================
 # data QC
@@ -86,11 +81,6 @@ wc -l data/meta/raw_read_paths/*txt
 
 # join file paths together into single file
 for x in 1 2 3 b; do paste data/meta/raw_read_paths/oligo_${x}_R1.txt data/meta/raw_read_paths/oligo_${x}_R2.txt > data/meta/raw_read_paths/oligo_${x}_R1R2.txt; done
-
-# do the same process for streptavidin data
-ls -C data/raw/streptavidin/*_R1_001.fastq.gz > data/meta/raw_read_paths/strep_R1.txt
-ls -C data/raw/streptavidin/*_R2_001.fastq.gz > data/meta/raw_read_paths/strep_R2.txt
-paste data/meta/raw_read_paths/strep_R1.txt data/meta/raw_read_paths/strep_R2.txt > data/meta/raw_read_paths/strep_R1R2.txt
 
 # creating the fastp command loop
 mkdir -p data/qc/fastp/
@@ -130,25 +120,6 @@ done > cmds/fastp_oligos.cmds
 # run 12 fastp commands at a time
 cat cmds/fastp_oligos.cmds | parallel -j12
 
-# now run full loop for streptavidin
-# makes file containing all fastp commands that need to be run for streptavidin
-while IFS=$'\t' read -r -a y; do
-	o1="$(basename ${y[0]} .fastq.gz)"
-	o2="$(basename ${y[1]} .fastq.gz)"
-	merg="${o1%_L00*}.merged.fpqc.fastq.gz"
-	echo "fastp -i ${y[0]} -I ${y[1]} \
-	-o data/qc/streptavidin/${o1}.fpqc.fastq.gz \
-	-O data/qc/streptavidin/${o2}.fpqc.fastq.gz \
-	--failed_out data/qc/fastp/failed/${o1%_L00*}.failed.txt \
-	--merge --merged_out data/merged/streptavidin/${merg} \
-	--length_limit 100 \
-	--json data/qc/fastp/strep_${o1%_L00*}.json \
-	--html data/qc/fastp/strep_${o1%_L00*}.html"
-done < data/meta/raw_read_paths/strep_R1R2.txt > cmds/fastp_strep.cmds
-
-# run 10 fastp commands at a time
-cat cmds/fastp_strep.cmds | parallel -j12
-
 # >>>>>>>>>>>>>>>>>>>>>>>
 # running multiqc [02/2024]
 # >>>>>>>>>>>>>>>>>>>>>>>
@@ -157,9 +128,6 @@ cat cmds/fastp_strep.cmds | parallel -j12
 
 # generate multiqc reports for oligos
 for x in 1 2 3 b; do multiqc data/qc/fastp/oligo_${x}* --filename data/qc/multiqc/oligo_${x}_merged --interactive --force; done
-
-# run multiqc for streptavidin
-multiqc data/qc/fastp/strep* --filename data/qc/multiqc/strep_merged --interactive --force
 
 # =======================================
 # renaming (organizing) merged reads +
@@ -177,12 +145,6 @@ for x in 1 2 3 b; do
 	grep -n '.*' <<< `ls data/merged/oligo_${x}/*3w*` > data/meta/merged_read_paths/oligo_${x}_3w_file_paths.txt
 done
 
-#  get paths & number replicates for each experiment (streptavidin)
-grep -n '.*' <<< `ls data/merged/streptavidin/*SA-Probe*` > data/meta/merged_read_paths/streptavidin_bg_file_paths.txt
-grep -n '.*' <<< `ls data/merged/streptavidin/*0w*` > data/meta/merged_read_paths/streptavidin_0w_file_paths.txt
-grep -n '.*' <<< `ls data/merged/streptavidin/*3w*` > data/meta/merged_read_paths/streptavidin_3w_file_paths.txt
-grep -n '.*' <<< `ls data/merged/streptavidin/*7w*` > data/meta/merged_read_paths/streptavidin_7w_file_paths.txt
-
 # >>>>>>>>>>>>>>>>>>>>>>>
 # background data
 # >>>>>>>>>>>>>>>>>>>>>>>
@@ -196,14 +158,6 @@ for x in 1 2 3 b; do
 done < data/meta/merged_read_paths/oligo_${x}_bg_file_paths.txt
 done
 
-# streptavidin
-while IFS=$':' read -r -a y; do
-	rep_num=${y[0]}
-	file=${y[1]}
-	new_name="streptavidin_bg_rep${rep_num}.merged.fasta"
-	zcat $file | sed -n '1~4s/^@/>/p;2~4p' > data/processed/streptavidin/${new_name}
-done < data/meta/merged_read_paths/streptavidin_bg_file_paths.txt
-
 # >>>>>>>>>>>>>>>>>>>>>>>
 # 0w wash data
 # >>>>>>>>>>>>>>>>>>>>>>>
@@ -216,14 +170,6 @@ for x in 1 2 3 b; do
 	zcat $file | sed -n '1~4s/^@/>/p;2~4p' > data/processed/oligo_${x}/${new_name}
 done < data/meta/merged_read_paths/oligo_${x}_0w_file_paths.txt
 done
-
-# streptavidin
-while IFS=$':' read -r -a y; do
-	rep_num=${y[0]}
-	file=${y[1]}
-	new_name="streptavidin_0w_rep${rep_num}.merged.fasta"
-	zcat $file | sed -n '1~4s/^@/>/p;2~4p' > data/processed/streptavidin/${new_name}
-done < data/meta/merged_read_paths/streptavidin_0w_file_paths.txt
 
 # >>>>>>>>>>>>>>>>>>>>>>>
 # 1w wash data
@@ -251,25 +197,6 @@ for x in 1 2 3 b; do
 done < data/meta/merged_read_paths/oligo_${x}_3w_file_paths.txt
 done
 
-# streptavidin
-while IFS=$':' read -r -a y; do
-	rep_num=${y[0]}
-	file=${y[1]}
-	new_name="streptavidin_3w_rep${rep_num}.merged.fasta"
-	zcat $file | sed -n '1~4s/^@/>/p;2~4p' > data/processed/streptavidin/${new_name}
-done < data/meta/merged_read_paths/streptavidin_3w_file_paths.txt
-
-# >>>>>>>>>>>>>>>>>>>>>>>
-# 7w data
-# >>>>>>>>>>>>>>>>>>>>>>>
-# streptavidin
-while IFS=$':' read -r -a y; do
-	rep_num=${y[0]}
-	file=${y[1]}
-	new_name="streptavidin_7w_rep${rep_num}.merged.fasta"
-	zcat $file | sed -n '1~4s/^@/>/p;2~4p' > data/processed/streptavidin/${new_name}
-done < data/meta/merged_read_paths/streptavidin_7w_file_paths.txt
-
 # =======================================
 # generate de Bruijn graphs for oligos
 # [02/2024 - 03/2024]
@@ -284,7 +211,7 @@ script_dir="/stor/work/Marcotte/project/rmcox/deBruijn_graphs/scripts"
 
 # low memory, quick (minutes to hours)
 for k in {5..15}; do
-for dir in oligo_1 oligo_2 oligo_3 oligo_b streptavidin; do
+for dir in oligo_1 oligo_2 oligo_3 oligo_b; do
 	for infile in `ls data/processed/${dir}`; do
 	outfile=${infile%.merged*}_${k}mers.csv
 	echo "python3 ${script_dir}/generate_deBruijn_kmers.py \
@@ -302,7 +229,7 @@ cat cmds/generate_dbgs.sh | parallel -j36
 
 # memory intensive, takes awhile (hours to days)
 for k in {5..15}; do
-for dir in oligo_1 oligo_2 oligo_3 oligo_b streptavidin; do
+for dir in oligo_1 oligo_2 oligo_3 oligo_b; do
 	for infile in `ls results/${dir}/graphs/*${k}mers.csv`; do
 	name="$(basename $infile .csv)"
 	outfile="${name}_counts.csv"
@@ -322,7 +249,7 @@ cat cmds/generate_kmer_counters.sh | parallel -j3
 
 # make dir for filtered k-mer set
 # make dirs for combined results
-for dir in oligo_1 oligo_2 oligo_3 oligo_b streptavidin; do
+for dir in oligo_1 oligo_2 oligo_3 oligo_b; do
 	mkdir results/${dir}/counts_combined_nolib/
 done
 
@@ -338,7 +265,7 @@ python3 ${script_dir}/label_kmers.py \
 --remove_lib --drop_label_col
 
 # command generator lib
-for exp in oligo_1 oligo_2 oligo_3 oligo_b streptavidin; do
+for exp in oligo_1 oligo_2 oligo_3 oligo_b; do
 	for k in {5..15}; do
 	for file in `ls results/${exp}/counts_combined/*_${k}mers_*wide*`; do
 	fname="$(basename ${file} .csv)"
@@ -358,16 +285,9 @@ cat cmds/remove_lib_kmers.cmds | parallel -j16
 # >>>>>>>>>>>>>>>>>>>>>>>
 
 # make dirs for new data
-for dir in oligo_1 oligo_2 oligo_3 oligo_b streptavidin; do
+for dir in oligo_1 oligo_2 oligo_3 oligo_b; do
 	mkdir results/${dir}/enrichment_nolib/
 done
-
-# template/test
-script_dir="/stor/work/Marcotte/project/rmcox/deBruijn_graphs/scripts"
-Rscript ${script_dir}/run_degust.R \
---counts_file results/streptavidin/counts_combined_nolib/streptavidin_10mers_combined_10c_wide_nolib.csv \
---exp 0w,3w \
---output_file results/streptavidin/enrichment_nolib/streptavidin_10mers_10c_all_washes_enriched_nolib.csv
 
 # command loop (run test for all washes combined)
 for k in {5..15}; do
@@ -380,34 +300,12 @@ done
 done > cmds/calc_enrich_all_wash_oligos_nolib.cmds
 cat cmds/calc_enrich_all_wash_oligos_nolib.cmds | parallel -j16
 
-# command loop (all washes, streptavidin)
-for k in {5..15}; do
-	echo "Rscript ${script_dir}/run_degust.R \
-	--counts_file results/streptavidin/counts_combined_nolib/streptavidin_${k}mers_combined_10c_wide_nolib.csv \
-	--exp 0w,3w \
-	--output_file results/streptavidin/enrichment_nolib/streptavidin_${k}mers_10c_all_washes_enriched_nolib.csv | tee -a logs/streptavidin_${k}mers_all_wash_enrichment_nolib.log"
-done > cmds/calc_enrich_all_wash_streptavidin_nolib.cmds
-cat cmds/calc_enrich_all_wash_streptavidin_nolib.cmds | parallel -j16
-
-
 # >>>>>>>>>>>>>>>>>>>>>>>
 # label data for plots
 # >>>>>>>>>>>>>>>>>>>>>>>
 
-# make dirs for new data
-for dir in oligo_1 oligo_2 oligo_3 oligo_b streptavidin chiton pfao glass; do
-	mkdir -p results/${dir}/labeled_nolib/
-done
-
-# get sanchita's chiton, pfao, glass data
-ln -s /stor/work/Ellington/sanchita/chitin_PFAO/results/Chitin_Only/labeled_nolib/*10c_all_washes_enriched_nolib_labeled.csv results/chiton/labeled_nolib/
-
-ln -s /stor/work/Ellington/sanchita/chitin_PFAO/results/Chitin_10mMPFAO/labeled_nolib/*10c_all_washes_enriched_nolib_labeled.csv results/pfao/labeled_nolib/
-
-ln -s /stor/work/Ellington/sanchita/glass161/results/glass161/labeled_nolib/*10c_all_washes_enriched_nolib_labeled.csv results/glass/labeled_nolib/
-
 # command generator loop
-for exp in oligo_1 oligo_2 oligo_3 oligo_b streptavidin; do
+for exp in oligo_1 oligo_2 oligo_3 oligo_b; do
 	for k in {5..15}; do
 	for file in `ls results/${exp}/enrichment_nolib/*_${k}mers_*`; do
 	fname="$(basename ${file} .csv)"
@@ -428,15 +326,12 @@ cat cmds/label_enriched_kmers_nolib.cmds | parallel -j32
 script_dir="/stor/work/Marcotte/project/rmcox/deBruijn_graphs/scripts"
 
 # make dirs for new data
-for dir in oligo_1 oligo_2 oligo_3 oligo_b streptavidin pfao chiton glass; do
+for dir in oligo_1 oligo_2 oligo_3 oligo_b; do
 	mkdir -p results/${dir}/consensus_seqs_nolib/
 done
 
-# template/test:
-python3 ${script_dir}/calc_consensus_seq.py --input_file results/oligo_3/consensus_seqs_nolib/oligo_3_9mers_weights.csv --calc_n 100 --edges
-
 # consensus seq command loop
-for exp in oligo_1 oligo_2 oligo_3 oligo_b streptavidin pfao chiton glass; do
+for exp in oligo_1 oligo_2 oligo_3 oligo_b; do
 	for k in {5..15}; do
 	echo "python3 ${script_dir}/calc_consensus_seq.py --input_file results/${exp}/consensus_seqs_nolib/${exp}_${k}mers_weights.csv --calc_n 100 --edges"
 done
